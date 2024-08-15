ce8bb732369476f0f6cec0c9f31249c98068786b
Taras Gordiienko <lozuvatka@gmail.com>
2024-08-15 09:25:28 +0100

    fix: fix the cors

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 with open('actions.json') as json_file:
     data = json.load(json_file)
